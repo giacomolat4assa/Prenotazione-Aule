07eb9127340f2b37fc094b65449bd8b25b586c5c
giacomolat4assa <giacomo.latassa06@gmail.com>
2025-06-20 18:18:08 +0200

    -

--- Prenotazioni_Aule/src/SistemaPrenotazioniArrayList.java
+++ Prenotazioni_Aule/src/SistemaPrenotazioniArrayList.java
@@ -1,4 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class SistemaPrenotazioniArrayList {
-
+	private List<Prenotazione> prenotazioni;
+	
+	public SistemaPrenotazioniArrayList() {
+		this.prenotazioni = new ArrayList<>();
+	}
+	
+	public void addPrenotazione(Prenotazione p) {
+		if(prenotazioni.contains(p))
+			throw new AulaException("prenotazione gia presente");
+		prenotazioni.add(p);
+	}
+	
+	public List<AulaStandard> getAuleinData (String dataPrenotazione){
+		List<AulaStandard> result = new ArrayList<>();
+		
+		for(Prenotazione p : prenotazioni) {
+			if(p.getData().equals(dataPrenotazione) && p.getAula() instanceof AulaStandard)
+				result.add( (AulaStandard) p.getAula() );
+		}
+		return result;
+	}
 }
