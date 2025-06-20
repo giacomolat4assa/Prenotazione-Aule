8bcb052e0f8e22df70508a3e20c3b331ac99cbcf
giacomolat4assa <giacomo.latassa06@gmail.com>
2025-06-20 13:49:51 +0200

    modifica metodo hashCode

---
+++ Prenotazioni_Aule/src/Aula.java
@@ -0,0 +1,32 @@
+import java.util.Objects;
+
+public abstract class Aula {
+	private String nome;
+	private int capienzaMax;
+	
+	Aula(String nome, int capienzaMax){
+		this.nome = nome;
+		this.capienzaMax = capienzaMax;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	public int getCapienzaMax() {
+		return this.capienzaMax;
+	}
+	
+	@Override
+	public boolean equals (Object obj) {
+		if(this == obj)
+			return true;
+		if(obj == null || this.getClass() != obj.getClass())
+			return false;
+		Aula other = (Aula) obj;
+		return Objects.equals(nome,other.nome) && (this.capienzaMax==other.capienzaMax);
+	}
+	@Override 
+	public int hashCode() {
+		return Objects.hash(nome,capienzaMax);
+	}
+}
