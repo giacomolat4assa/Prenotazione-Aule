293f8727003a548d7edfe739a3cf584e6986a50a
giacomolat4assa <giacomo.latassa06@gmail.com>
2025-06-20 13:50:48 +0200

    modifica fatta

---
+++ Prenotazioni_Aule/src/AulaStandard.java
@@ -0,0 +1,27 @@
+import java.util.Objects;
+
+public class AulaStandard extends Aula{
+	private int numeroLavagne;
+	
+	public AulaStandard(String nome, int capienzaMax, int numeroLavagne){
+		super(nome, capienzaMax);
+		this.numeroLavagne=numeroLavagne;
+	}
+	
+	public int getNumeroLavagne() {
+		return numeroLavagne;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if(!super.equals(obj))
+			return false;
+		if(this.getClass() != obj.getClass())
+			return false;
+		AulaStandard other = (AulaStandard) obj;
+		return this.numeroLavagne == other.numeroLavagne;
+	}
+	@Override 
+	public int hashCode() {
+		return Objects.hash(super.hashCode(), numeroLavagne);
+	}
+}
